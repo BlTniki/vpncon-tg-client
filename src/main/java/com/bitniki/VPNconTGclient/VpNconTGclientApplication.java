8e3dd5c8874bd005ca1b6056197904d2d71b5282
BITniki <shakarimovdima@ya.ru>
2022-12-09 18:33:58 +0300

    init

---
+++ src/main/java/com/bitniki/VPNconTGclient/VpNconTGclientApplication.java
@@ -0,0 +1,13 @@
+package com.bitniki.VPNconTGclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpNconTGclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpNconTGclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitniki/VPNconTGclient/VpNconTGclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitniki.VPNconTGclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpNconTGclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
