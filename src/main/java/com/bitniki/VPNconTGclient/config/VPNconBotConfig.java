1357ea78c905a0ba2852850416a8085ff0f7fbb1
BITniki <shakarimovdima@ya.ru>
2022-12-10 23:06:25 +0300

    init VPNcon configuration it's configure tg bot and run updates listener

---
+++ src/main/java/com/bitniki/VPNconTGclient/config/VPNconBotConfig.java
@@ -0,0 +1,36 @@
+package com.bitniki.VPNconTGclient.config;
+
+
+import com.bitniki.VPNconTGclient.bot.ChatEchoBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class VPNconBotConfig {
+    @Value("${tg.botToken}")
+    private String botToken;
+
+    @Value("${tg.botUsername}")
+    private String botUsername;
+
+    /**
+     * init bot and run updates listener
+     * @return TelegramBotsApi
+     * @throws TelegramApiException
+     */
+    @Bean
+    public TelegramBotsApi telegramBotsApiBean() throws TelegramApiException {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new ChatEchoBot(botToken, botUsername));
+            return botsApi;
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
