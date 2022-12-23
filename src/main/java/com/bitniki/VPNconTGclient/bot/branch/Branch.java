e6bea89d292e61c87507da416fc479320361603b
BITniki <shakarimovdima@ya.ru>
2022-12-23 23:05:58 +0300

    Basic realization of auth dialogue

--- src/main/java/com/bitniki/VPNconTGclient/bot/branch/Branch.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/branch/Branch.java
@@ -1,14 +1,33 @@
 package com.bitniki.VPNconTGclient.bot.branch;
 
 import com.bitniki.VPNconTGclient.bot.Responses;
+import com.bitniki.VPNconTGclient.service.RequestService;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public abstract class Branch {
     private Branch prevBranch;
+    private RequestService requestService;
 
-    public Branch(Branch prevBranch) {
+    public Branch(Branch prevBranch, RequestService requestService) {
         this.prevBranch = prevBranch;
+        this.requestService = requestService;
     }
 
     public abstract Responses handle(Update update);
+
+    public Branch getPrevBranch() {
+        return prevBranch;
+    }
+
+    public void setPrevBranch(Branch prevBranch) {
+        this.prevBranch = prevBranch;
+    }
+
+    public RequestService getRequestService() {
+        return requestService;
+    }
+
+    public void setRequestService(RequestService requestService) {
+        this.requestService = requestService;
+    }
 }
--- src/main/java/com/bitniki/VPNconTGclient/bot/branch/InitBranch.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/branch/InitBranch.java
@@ -4,21 +4,29 @@ import com.bitniki.VPNconTGclient.bot.Response;
 import com.bitniki.VPNconTGclient.bot.ResponseType;
 import com.bitniki.VPNconTGclient.bot.Responses;
 import com.bitniki.VPNconTGclient.requestEntity.UserEntity;
+import com.bitniki.VPNconTGclient.service.RequestService;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.HttpClientErrorException;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ForceReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class InitBranch extends Branch{
+    private final String initText = "Привет! Представься пожалуста.";
     private final String loginText = "Hi! Type your login";
     private final String passwordText = "Great! now type your password";
     private final String entityText = "Awesome! Your user is:\n";
     private UserEntity userEntity;
 
-    public InitBranch(Branch prevBranch) {
-        super(prevBranch);
+    public InitBranch(Branch prevBranch, RequestService requestService) {
+        super(prevBranch, requestService);
     }
 
     @Override
@@ -30,26 +38,60 @@ public class InitBranch extends Branch{
         responses.setResponseList(new ArrayList<>());
 
         //init branch state
-        if(message.getReplyToMessage() == null) {
-            SendMessage sendMessage = new SendMessage(message.getChatId().toString(), loginText);
-            sendMessage.setReplyMarkup(new ForceReplyKeyboard(true));
+        //send hi message and set nav buttons
+        if(message.getText().equals("/start")) {
+            try {
+                this.userEntity = getRequestService().getUserByTelegramId(message.getFrom().getUserName());
+                SendMessage sendMessage = new SendMessage(message.getChatId().toString(), entityText
+                        + userEntity.getLogin()
+                );
+                responses.getResponseList().add(new Response<SendMessage>(ResponseType.SendText, sendMessage));
+                return responses;
+            } catch (HttpClientErrorException e) {
+                if(!e.getStatusCode().is4xxClientError()) throw e;
+            }
+            SendMessage sendMessage = new SendMessage(message.getChatId().toString(), initText);
+            KeyboardRow keyboardRow = new KeyboardRow(List.of(
+                    new KeyboardButton("Регистрация"),
+                    new KeyboardButton("Авторизация")
+            ));
+            ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(List.of(keyboardRow));
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
             responses.getResponseList().add(new Response<SendMessage>(ResponseType.SendText, sendMessage));
             userEntity = new UserEntity();
             return responses;
         }
         //login typed branch state
-        if(message.getReplyToMessage().getText().equals(loginText)) {
-            userEntity.setUsername(message.getText());
+        if(message.getText().equals("Авторизация")) {
+            SendMessage sendMessage = new SendMessage(message.getChatId().toString(), loginText);
+            sendMessage.setReplyMarkup(new ForceReplyKeyboard(true));
+            responses.getResponseList().add(new Response<SendMessage>(ResponseType.SendText, sendMessage));
+        }
+        //login typed branch state
+        if(message.getReplyToMessage()!= null && message.getReplyToMessage().getText().equals(loginText)) {
+            userEntity.setLogin(message.getText());
             SendMessage sendMessage = new SendMessage(message.getChatId().toString(), passwordText);
             sendMessage.setReplyMarkup(new ForceReplyKeyboard(true));
             responses.getResponseList().add(new Response<SendMessage>(ResponseType.SendText, sendMessage));
         }
         //password typed branch state
-        if(message.getReplyToMessage().getText().equals(passwordText)) {
+        if(message.getReplyToMessage()!= null && message.getReplyToMessage().getText().equals(passwordText)) {
             userEntity.setPassword(message.getText());
+            userEntity.setTelegramId(message.getFrom().getUserName());
+            try {
+                getRequestService().associateTelegramIdWithUser(userEntity);
+            } catch (HttpClientErrorException e) {
+                if(!e.getStatusCode().is4xxClientError()) throw e;
+                SendMessage sendMessage = new SendMessage(message.getChatId().toString(), e.getMessage());
+                responses.getResponseList().add(new Response<SendMessage>(ResponseType.SendText, sendMessage));
+                return responses;
+            }
+
             SendMessage sendMessage = new SendMessage(message.getChatId().toString(), entityText
-                    + userEntity.getUsername()
-                    + "\n" + userEntity.getPassword());
+                    + userEntity.getLogin()
+                    + "\n" + userEntity.getPassword()
+                    + "\n" + userEntity.getTelegramId());
             responses.getResponseList().add(new Response<SendMessage>(ResponseType.SendText, sendMessage));
         }
 
