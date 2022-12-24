fdc7260e381dffc4459077b9394b3ae3f8dbf2f8
BITniki <shakarimovdima@ya.ru>
2022-12-24 19:14:14 +0300

    Init

---
+++ src/main/java/com/bitniki/VPNconTGclient/exception/notFoundException/ActivateTokenNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bitniki.VPNconTGclient.exception.notFoundException;
+
+public class ActivateTokenNotFoundException extends EntityNotFoundException{
+    public ActivateTokenNotFoundException(String message) {
+        super(message);
+    }
+}
