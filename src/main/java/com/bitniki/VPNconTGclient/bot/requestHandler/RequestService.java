07542da1499f6342214bff4b7e3f397bcc7cffca
BITniki <shakarimovdima@ya.ru>
2023-01-26 22:34:05 +0300

    added createPeerOnServer() method

--- src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/RequestService.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/RequestService.java
@@ -1,9 +1,12 @@
 package com.bitniki.VPNconTGclient.bot.requestHandler;
 
+import com.bitniki.VPNconTGclient.bot.exception.notFoundException.EntityNotFoundException;
 import com.bitniki.VPNconTGclient.bot.exception.requestHandlerException.RequestService5xxException;
 import com.bitniki.VPNconTGclient.bot.exception.requestHandlerException.RequestServiceException;
 import com.bitniki.VPNconTGclient.bot.exception.notFoundException.UserNotFoundException;
+import com.bitniki.VPNconTGclient.bot.exception.validationFailedException.EntityValidationFailedException;
 import com.bitniki.VPNconTGclient.bot.exception.validationFailedException.UserValidationFailedException;
+import com.bitniki.VPNconTGclient.bot.requestHandler.requestEntity.PeerEntity;
 import com.bitniki.VPNconTGclient.bot.requestHandler.requestEntity.TokenEntity;
 import com.bitniki.VPNconTGclient.bot.requestHandler.requestEntity.UserEntity;
 import org.springframework.http.HttpEntity;
@@ -103,6 +106,33 @@ public class RequestService {
         return response.getBody();
     }
 
+    public PeerEntity createPeerOnServer(PeerEntity peerEntity, Long userId, Long hostId)
+            throws EntityNotFoundException, EntityValidationFailedException, RequestService5xxException {
+        String uri = this.VPNconAddress + "/peers?user_id=" + userId + "&host_id=" + hostId;
+        //Configure request body
+        HttpEntity<PeerEntity> httpEntity = makeHttpEntity(peerEntity);
+        //Configure response entity
+        ResponseEntity<PeerEntity> response;
+
+        //Make request
+        try {
+            response = restTemplate.postForEntity(
+                    uri,
+                    httpEntity,
+                    PeerEntity.class
+            );
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode().value() == 400)
+                throw new EntityValidationFailedException(e.getMessage());
+            if(e.getStatusCode().value() == 404)
+                throw new EntityNotFoundException(e.getMessage());
+            if(e.getStatusCode().is5xxServerError())
+                throw new RequestService5xxException("Problems with server occurred");
+            throw e;
+        }
+        return response.getBody();
+    }
+
     /*
     Make request for auth token and save it
      */
@@ -135,9 +165,15 @@ public class RequestService {
     /*
     Configure request body for user entity
      */
-    private HttpEntity<UserEntity> makeHttpEntity(UserEntity userEntity) {
+    private HttpEntity<UserEntity> makeHttpEntity(UserEntity entity) {
         return new HttpEntity<>(
-                userEntity,
+                entity,
+                httpHeaders
+        );
+    }
+    private HttpEntity<PeerEntity> makeHttpEntity(PeerEntity entity) {
+        return new HttpEntity<>(
+                entity,
                 httpHeaders
         );
     }
