c834be7ee25744243a172bad31681877449537df
BITniki <shakarimovdima@ya.ru>
2023-01-26 19:21:59 +0300

    init

---
+++ src/main/java/com/bitniki/VPNconTGclient/bot/exception/BranchCriticalException.java
@@ -0,0 +1,10 @@
+package com.bitniki.VPNconTGclient.bot.exception;
+
+/*
+This exception assumes reinitialization dialogue tree
+ */
+public class BranchCriticalException extends Exception {
+    public BranchCriticalException(String message) {
+        super(message);
+    }
+}
