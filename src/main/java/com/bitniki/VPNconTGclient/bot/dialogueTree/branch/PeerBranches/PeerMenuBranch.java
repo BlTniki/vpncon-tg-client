3c9813401997c410156b43156ad4e1a13b645834
BITniki <shakarimovdima@ya.ru>
2023-01-26 22:33:27 +0300

    added route to create peer branch

--- src/main/java/com/bitniki/VPNconTGclient/bot/dialogueTree/branch/PeerBranches/PeerMenuBranch.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/dialogueTree/branch/PeerBranches/PeerMenuBranch.java
@@ -1,5 +1,6 @@
 package com.bitniki.VPNconTGclient.bot.dialogueTree.branch.PeerBranches;
 
+import com.bitniki.VPNconTGclient.bot.dialogueTree.branch.Branch;
 import com.bitniki.VPNconTGclient.bot.dialogueTree.branch.BranchWithUser;
 import com.bitniki.VPNconTGclient.bot.requestHandler.RequestService;
 import com.bitniki.VPNconTGclient.bot.response.Response;
@@ -15,11 +16,20 @@ import java.util.List;
 @SuppressWarnings({"FieldCanBeLocal", "FieldMayBeFinal"})
 public class PeerMenuBranch extends BranchWithUser {
     private enum BranchState {
-        InitState()
+        InitState(),
+        WaitingForButtonChoose()
     }
     private BranchState branchState;
+    private final String initText = "Итак меню VPN:";
+    private final String createText = "Создать новый конфиг";
+    private final String editText = "Показать созданные";
     private final String errorText = "Похоже на внутренюю ошибку бота. " +
             "Напиши мне: @BITniki";
+
+    public PeerMenuBranch(Branch prevBranch, RequestService requestService) {
+        super(prevBranch, requestService);
+        this.branchState = BranchState.InitState;
+    }
 
     public PeerMenuBranch(BranchWithUser prevBranch, RequestService requestService) {
         super(prevBranch, requestService);
@@ -27,17 +37,52 @@ public class PeerMenuBranch extends BranchWithUser {
     }
 
     @Override
-    public List<Response<?>> makeResponses(Update update) throws BranchBadUpdateProvidedException {
+    public List<Response<?>> makeResponses(Update update) {
         //Get message from update
         Message message = update.getMessage();
 
         //Init state
         if(branchState.equals(BranchState.InitState)) {
-            return returnPeersList(message);
+            return providePeerMenu(message);
+        }
+
+        //WaitingForButtonChoose state
+        if(branchState.equals(BranchState.WaitingForButtonChoose)) {
+            if(message.getText().equals(createText)) {
+                return routeToCreateBranch();
+            }
+
         }
 
         //If we got here return null
         return null;
+    }
+
+    private List<Response<?>> providePeerMenu(Message message) {
+        //Init Responses
+        List<Response<?>> responses = new ArrayList<>();
+
+        SendMessage sendMessage = new SendMessage(message.getChatId().toString(),
+                initText
+        );
+        //set nav buttons
+        sendMessage.setReplyMarkup(makeKeyboardMarkupWithMainButton(createText, editText));
+        responses.add(new Response<>(ResponseType.SendText, sendMessage));
+
+        //Change branch state
+        branchState = BranchState.WaitingForButtonChoose;
+
+        return  responses;
+    }
+
+    private List<Response<?>> routeToCreateBranch() {
+        //Init Responses
+        List<Response<?>> responses = new ArrayList<>();
+
+        //Change branch
+        setNextBranch(new CreatePeerBranch(this, requestService));
+
+        return responses;
     }
 
     private List<Response<?>> returnPeersList(Message message) throws BranchBadUpdateProvidedException {
