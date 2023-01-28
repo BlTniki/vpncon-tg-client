de63818e196fe87b0417f865c9ee428699a0945b
BITniki <shakarimovdima@ya.ru>
2023-01-28 20:48:30 +0300

    added askPeerIp method

--- src/main/java/com/bitniki/VPNconTGclient/bot/dialogueTree/branch/PeerBranches/CreatePeerBranch.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/dialogueTree/branch/PeerBranches/CreatePeerBranch.java
@@ -7,16 +7,18 @@ import com.bitniki.VPNconTGclient.bot.exception.requestHandlerException.RequestS
 import com.bitniki.VPNconTGclient.bot.exception.requestHandlerException.RequestServiceException;
 import com.bitniki.VPNconTGclient.bot.requestHandler.RequestService;
 import com.bitniki.VPNconTGclient.bot.requestHandler.requestEntity.HostEntity;
+import com.bitniki.VPNconTGclient.bot.requestHandler.requestEntity.PeerEntity;
 import com.bitniki.VPNconTGclient.bot.response.Response;
 import com.bitniki.VPNconTGclient.bot.response.ResponseType;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ForceReplyKeyboard;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("FieldCanBeLocal")
+@SuppressWarnings({"FieldCanBeLocal", "TextBlockMigration"})
 public class CreatePeerBranch extends BranchWithUser {
     private enum BranchState {
         InitState(),
@@ -24,7 +26,13 @@ public class CreatePeerBranch extends BranchWithUser {
     }
     private BranchState branchState;
 
+    private PeerEntity peerEntity;
+
     private final String hostListText = "Выбери локацию сервера:";
+    private final String availablePeersText ="/254 доступно";
+    private final String askPeerIpText = "Веберем тебе айпишник." +
+            "\nНапиши число 0, если тебе неважно." +
+            "\nНапиши число от 2 до 254 и мы проверим доступность.";
 
     public CreatePeerBranch(Branch prevBranch, RequestService requestService) {
         super(prevBranch, requestService);
@@ -47,6 +55,12 @@ public class CreatePeerBranch extends BranchWithUser {
             return provideHostList(message);
         }
 
+        //WaitingForHostChoose state
+        if(branchState.equals(BranchState.WaitingForHostChoose)) {
+            return askPeerIp(message);
+        }
+
+        //if we got here
         return null;
     }
 
@@ -62,7 +76,7 @@ public class CreatePeerBranch extends BranchWithUser {
         List<HostEntity> hostList = requestService.getHostsFromServer();
         String[] hostListNames = hostList
                 .stream()
-                .map(HostEntity::getName)
+                .map(host -> host.getName() + "\n" + host.getAvailablePeersCount() + availablePeersText)
                 .toList()
                 .toArray(new String[0]);
         //set nav buttons
@@ -71,6 +85,32 @@ public class CreatePeerBranch extends BranchWithUser {
         );
         responses.add(new Response<>(ResponseType.SendText, sendMessage));
 
+        //Change branch state
+        branchState = BranchState.WaitingForHostChoose;
+        return responses;
+    }
+    private List<Response<?>> askPeerIp(Message message)
+            throws RequestService5xxException, BranchBadUpdateProvidedException {
+        //Init Responses
+        List<Response<?>> responses = new ArrayList<>();
+
+        //Validate host name and get host entity
+        String hostName = message.getText().substring(
+                0,
+                message.getText().indexOf('\n') // Cut available peers text
+                );
+        //load host list and find host with given hostName or throw exception
+        HostEntity hostEntity = requestService.getHostsFromServer().stream()
+                .filter(host -> host.getName().equals(hostName)).findFirst()
+                .orElseThrow(() -> new BranchBadUpdateProvidedException("No such host"));
+        //init peerEntity and set host
+        peerEntity = new PeerEntity();
+        peerEntity.setHost(hostEntity);
+
+        SendMessage sendMessage = new SendMessage(message.getChatId().toString(), askPeerIpText);
+        //Force reply
+        sendMessage.setReplyMarkup(new ForceReplyKeyboard());
+        responses.add(new Response<>(ResponseType.SendText, sendMessage));
         return responses;
     }
 }
