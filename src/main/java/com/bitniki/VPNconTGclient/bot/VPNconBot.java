67cb20b402b3db1557b4d79e2fb900b68daaf66e
BITniki <shakarimovdima@ya.ru>
2023-02-09 23:45:41 +0300

    Refactor executing

--- src/main/java/com/bitniki/VPNconTGclient/bot/VPNconBot.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/VPNconBot.java
@@ -1,12 +1,14 @@
 package com.bitniki.VPNconTGclient.bot;
 
 import com.bitniki.VPNconTGclient.bot.response.Response;
+import com.bitniki.VPNconTGclient.bot.response.ResponseType;
 import com.bitniki.VPNconTGclient.bot.router.UpdateRouter;
 import com.bitniki.VPNconTGclient.bot.exception.UpdateRouterException;
 import org.telegram.telegrambots.meta.api.methods.AnswerPreCheckoutQuery;
 import org.telegram.telegrambots.meta.api.methods.invoices.SendInvoice;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -38,16 +40,32 @@ public class VPNconBot extends BasicTelegramBot{
 
         //Get all responses
         try {
-            for(Response<?> response : responses) {
-                switch (response.getResponseType()) {
-                    case SendText -> execute((SendMessage)response.getData());
-                    case SendDoc -> execute((SendDocument) response.getData());
-                    case SendInvoice -> sendInvoice((SendInvoice) response.getData());
-                    case SendAnswerToInvoice -> execute((AnswerPreCheckoutQuery)response.getData());
-                }
+            for (Response<?> response : responses) {
+                execute(response.getResponseType(), response.getData());
             }
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Executes the appropriate Telegram API method based on the provided {@link ResponseType}
+     *
+     * @param responseType The type of response to be executed
+     * @param data The data associated with the response type
+     * @throws TelegramApiException If there is an error executing the Telegram API method
+     */
+    private <T> void execute(ResponseType responseType, T data) throws TelegramApiException {
+        switch (responseType) {
+            case SendText -> execute((SendMessage) data);
+            case SendPhoto -> execute((SendPhoto) data);
+            case SendDoc -> execute((SendDocument) data);
+            case SendInvoice -> {
+                //set providerToken
+                ((SendInvoice) data).setProviderToken(paymentToken);
+                sendInvoice((SendInvoice) data);
+            }
+            case SendAnswerToInvoice -> execute((AnswerPreCheckoutQuery) data);
         }
     }
 
