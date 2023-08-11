3d02cda692699650c1c9f6c8e2e5d977ccfc1485
BITniki <shakarimovdima@ya.ru>
2023-08-11 12:35:23 +0300

    done RequestHandler implementation

--- src/main/java/com/bitniki/VPNconTGclient/config/RequestConfig.java
+++ src/main/java/com/bitniki/VPNconTGclient/config/RequestConfig.java
@@ -1,6 +1,8 @@
 package com.bitniki.VPNconTGclient.config;
 
-import com.bitniki.VPNconTGclient.bot.requestHandler.RequestService;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.exception.requestHandler.RequestHandlerException;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.requestHandle.RequestHandler;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.requestHandle.impl.RequestHandlerImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,30 +13,40 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class RequestConfig {
     @Value("${VPNconServer.address}")
-    private String VPNconAddress;
-    @Value("${VPNconServer.domain}")
-    private String VPNconDomain;
+    private String VPNCON_ADDRESS;
     @Value("${VPNconServer.login}")
-    private String botLogin;
+    private String BOT_LOGIN;
     @Value("${VPNconServer.password}")
-    private String botPassword;
+    private String BOT_PASSWORD;
 
-    private RequestService requestService;
+    private RequestHandler requestHandler;
 
-    /**
-     * Configure Request bean
-     */
     @Bean
-    public RequestService requestServiceBean() {
-        requestService = new RequestService(VPNconAddress, VPNconDomain, botLogin, botPassword);
-        return requestService;
+    public RequestHandler requestHandlerBean() {
+        this.requestHandler =  new RequestHandlerImpl(
+                VPNCON_ADDRESS,
+                BOT_LOGIN,
+                BOT_PASSWORD
+        );
+        return this.requestHandler;
     }
 
     /**
      * Update bot token every day at 00:00
      */
     @Scheduled(cron = "0 0 0 * * ?")
-    public void updateToken() {
-        requestService.refreshBotToken();
+    public void updateToken() throws RequestHandlerException {
+        requestHandler.SignInAndMakeHeaders();
     }
+
+//    private RequestService requestService;
+
+//    /**
+//     * Configure Request bean
+//     */
+//    @Bean
+//    public RequestService requestServiceBean() {
+//        requestService = new RequestService(VPNconAddress, VPNconDomain, botLogin, botPassword);
+//        return requestService;
+//    }
 }
