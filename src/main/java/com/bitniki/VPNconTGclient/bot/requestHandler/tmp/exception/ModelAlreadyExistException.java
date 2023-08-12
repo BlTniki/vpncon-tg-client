f1faa69175c0b7fb5d62c3b16acc9e09fd8405dd
BITniki <shakarimovdima@ya.ru>
2023-08-12 20:51:22 +0300

    remove redundant class

--- src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/tmp/exception/ModelAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.bitniki.VPNconTGclient.bot.requestHandler.tmp.exception;
-
-public class ModelAlreadyExistException extends Exception {
-    public ModelAlreadyExistException(String message) {
-        super(message);
-    }
-}
