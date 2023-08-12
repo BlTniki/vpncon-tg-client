e3eb7bba5153be30419c8df415b416495175bf3e
BITniki <shakarimovdima@ya.ru>
2023-08-12 17:57:08 +0300

    add countAvailablePeersOnHost feature

--- src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/tmp/RequestService/HostRequestService.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/tmp/RequestService/HostRequestService.java
@@ -1,6 +1,7 @@
 package com.bitniki.VPNconTGclient.bot.requestHandler.tmp.RequestService;
 
 import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.Model.impl.Host;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.exception.ModelNotFoundException;
 
 import java.util.List;
 
@@ -10,4 +11,12 @@ public interface HostRequestService {
      * @return Список хостов.
      */
     List<Host> getHostsFromServer();
+
+    /**
+     * Получение кол-ва доступных пиров на сервере.
+     * @param hostId Id хоста.
+     * @return Кол-во доступных пиров.
+     * @throws ModelNotFoundException Если хост не найден.
+     */
+    Integer countAvailablePeersOnHost(Long hostId) throws ModelNotFoundException;
 }
--- src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/tmp/RequestService/impl/HostRequestServiceImpl.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/tmp/RequestService/impl/HostRequestServiceImpl.java
@@ -2,6 +2,8 @@ package com.bitniki.VPNconTGclient.bot.requestHandler.tmp.RequestService.impl;
 
 import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.Model.impl.Host;
 import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.RequestService.HostRequestService;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.exception.ModelNotFoundException;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.exception.requestHandler.RequestHandler404Exception;
 import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.requestHandle.RequestHandler;
 
 import java.util.List;
@@ -18,5 +20,16 @@ public class HostRequestServiceImpl implements HostRequestService {
         final String ENDPOINT = "/hosts";
 
         return List.of(requestHandler.GET(ENDPOINT, Host[].class));
+    }
+
+    @Override
+    public Integer countAvailablePeersOnHost(Long hostId) throws ModelNotFoundException {
+        final String ENDPOINT = "/hosts/count/" + hostId;
+
+        try {
+            return requestHandler.GET(ENDPOINT, Integer.class);
+        } catch (RequestHandler404Exception e) {
+            throw new ModelNotFoundException("Не смог найти хост с id %d".formatted(hostId));
+        }
     }
 }
