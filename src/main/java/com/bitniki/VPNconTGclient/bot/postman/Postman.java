0e6f2a02eb2c9c1e0e370a31e4b68adff3375b61
BITniki <shakarimovdima@ya.ru>
2023-08-12 16:38:13 +0300

    implement new Request module

--- src/main/java/com/bitniki/VPNconTGclient/bot/postman/Postman.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/postman/Postman.java
@@ -1,9 +1,8 @@
 package com.bitniki.VPNconTGclient.bot.postman;
 
 import com.bitniki.VPNconTGclient.bot.BasicTelegramBot;
-import com.bitniki.VPNconTGclient.bot.exception.requestHandlerException.RequestService5xxException;
-import com.bitniki.VPNconTGclient.bot.requestHandler.RequestService;
-import com.bitniki.VPNconTGclient.bot.requestHandler.requestEntity.MailEntity;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.Model.impl.Reminder;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.RequestService.RequestServiceFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
@@ -12,22 +11,22 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class Postman {
     private final BasicTelegramBot bot;
-    private final RequestService requestService;
-    public Postman(BasicTelegramBot bot, RequestService requestService) {
+    private final RequestServiceFactory requestService;
+    public Postman(BasicTelegramBot bot, RequestServiceFactory requestService) {
         this.bot = bot;
         this.requestService = requestService;
     }
 
     @Scheduled(cron = "0 0 * * * ?")
-    public void deliverMails() throws RequestService5xxException {
+    public void deliverReminders() {
         //Get mails
-        List<MailEntity> mails = requestService.getMailsFromServer();
+        List<Reminder> reminders = requestService.REMINDER_REQUEST_SERVICE.getRemindersFromServer();
 
         //Send to users
-        for (MailEntity mail: mails) {
-            Long telegramId = mail.getUser().getTelegramId();
+        for (Reminder reminder: reminders) {
+            Long telegramId = reminder.getUser().getTelegramId();
             if(telegramId!=null) {
-                bot.sendMailToUser(telegramId, mail.getPayload());
+                bot.sendMailToUser(telegramId, reminder.getPayload());
             }
         }
     }
--- src/main/java/com/bitniki/VPNconTGclient/config/PostmanConfig.java
+++ src/main/java/com/bitniki/VPNconTGclient/config/PostmanConfig.java
@@ -2,23 +2,16 @@ package com.bitniki.VPNconTGclient.config;
 
 import com.bitniki.VPNconTGclient.bot.BasicTelegramBot;
 import com.bitniki.VPNconTGclient.bot.postman.Postman;
-import com.bitniki.VPNconTGclient.bot.requestHandler.RequestService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.RequestService.RequestServiceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SuppressWarnings("unused")
 @Configuration
 @EnableScheduling
 public class PostmanConfig {
-    @Autowired
-    private BasicTelegramBot bot;
-    @Autowired
-    private RequestService requestService;
-
     @Bean
-    public Postman postmanBean() {
+    public Postman postmanBean(BasicTelegramBot bot, RequestServiceFactory requestService) {
         return new Postman(bot, requestService);
     }
 }
