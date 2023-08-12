2914b07fc577b71e5e78830e3001fcb907b7a7fb
BITniki <shakarimovdima@ya.ru>
2023-08-12 20:12:25 +0300

    add field validation method

--- src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/tmp/Model/impl/PeerValidatorRegex.java
+++ src/main/java/com/bitniki/VPNconTGclient/bot/requestHandler/tmp/Model/impl/PeerValidatorRegex.java
@@ -3,6 +3,8 @@ package com.bitniki.VPNconTGclient.bot.requestHandler.tmp.Model.impl;
 import com.bitniki.VPNconTGclient.bot.requestHandler.tmp.Model.Model;
 import lombok.*;
 
+import java.util.regex.Pattern;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
@@ -11,4 +13,15 @@ import lombok.*;
 public class PeerValidatorRegex extends Model {
     private String peerIp;
     private String peerConfName;
+
+    /**
+     * Валидация названия конфига.
+     * @param peerConfName Название конфига.
+     * @return True, если валидация НЕ ПРОЙДЕНА.
+     */
+    public Boolean isPeerConfNameNotValid(String peerConfName) {
+        Pattern passwordPattern = Pattern.compile(this.peerConfName);
+
+        return !passwordPattern.matcher(peerConfName).matches();
+    }
 }
